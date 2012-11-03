7f19eceb4297c1a308be7c83d173898c1effe0d0
Anton Rogov <antonrogov@me.com>
2012-11-03 14:50:55 +0400

    use jquery's ajax method to load all.js

--- lib/jquery-facebook/version.rb
+++ lib/jquery-facebook/version.rb
@@ -1,5 +1,5 @@
 module Jquery
   module Facebook
-    VERSION = "0.0.1"
+    VERSION = "0.1.0"
   end
 end
--- vendor/assets/javascripts/jquery.facebook.js
+++ vendor/assets/javascripts/jquery.facebook.js
@@ -1,26 +1,25 @@
 $.facebookInit = function (options) {
-  var defaults = {
-    status: true,
-    cookie: true,
-    xfbml: true
-  };
+  var defaults = { status: true,
+                   cookie: true,
+                   xfbml: true },
+      meta = $('meta[property="fb:app_id"]');
 
-  var meta = $('meta[property="fb:app_id"]');
   if (meta.length) {
     defaults.appId = meta.attr('content');
   }
 
-  window.fbAsyncInit = function() {
+  window.fbAsyncInit = function () {
     FB.init($.extend(defaults, options));
     $(document).trigger('facebook:init');
   };
 
-  if (!$("div#fb-root").length) {
+  if (!$('div#fb-root').length) {
     $('body').append('<div id="fb-root"></div>');
   }
 
-  var e = document.createElement('script');
-  e.async = true;
-  e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-  document.getElementById('fb-root').appendChild(e);
-}
+  $.ajax({
+    url: document.location.protocol + '//connect.facebook.net/en_US/all.js',
+    dataType: 'script',
+    cache: true
+  });
+};
